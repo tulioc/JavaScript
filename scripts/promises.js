e6cf9f634daca3e53e9edadad9c66f0aa30308ce
Tulio Coelho <tulioopc@gmail.com>
2018-04-17 18:28:15 -0300

    Adicionando scripts simples para diferenciar promises de callbacks

---
+++ scripts/promises.js
@@ -0,0 +1,28 @@
+const getData = (callback) => {
+  setTimeout(() => {
+    callback(undefined, 'Deu bom! - Callback');
+  }, 2000);
+}
+
+getData((err, data) => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+
+
+
+const myPromise = new Promise( (resolve, reject) => {
+  setTimeout(() => {
+    resolve('Deu bom! - Promise');
+  }, 2000)
+});
+
+
+myPromise.then((data) => {
+  console.log(data)
+}, (err) => {
+  console.log(err);
+});
